cd68218b970515a22f6e062a0d4d2638a920cd34
boostmachine <184125960+boostmachine@users.noreply.github.com>
2025-07-10 00:02:47 +0300

    add buttons and basic functions

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+function add(a, b) {
+  return a + b
+};
+
+function subtract(a, b) {
+  return a - b
+};
+
+function multiply(a, b) {
+  return a*b
+};
+
+function divide(a, b) {
+  return a / b
+};
+
+let a = "";
+let b = "";
+let operator = "";
+
