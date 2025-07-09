38a0377720128421d8ab594737ffb7c16df8c88d
boostmachine <184125960+boostmachine@users.noreply.github.com>
2025-07-10 01:02:16 +0300

    add numbers and operators to script

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 "use strict";
 
+let a = "";
+let b = "";
+let operator = "";
+let display = document.querySelector(".display")
+let currentDisplay = ""
+//Numbers
+let one = document.getElementById("one")
+let two = document.getElementById("one")
+let three = document.getElementById("one")
+let four = document.getElementById("one")
+let five = document.getElementById("one")
+let six = document.getElementById("one")
+let seven = document.getElementById("one")
+let eight = document.getElementById("one")
+let nine = document.getElementById("one")
+let zero = document.getElementById("one")
+// Operators
+let plus = document.getElementById("plus")
+let minus = document.getElementById("minus")
+let asterisk = document.getElementById("asterisk")
+let slash = document.getElementById("slash")
+let equal = document.getElementById("equal")
+let clear = document.getElementById("clear")
+
+
 function add(a, b) {
   return a + b
 };
@@ -13,6 +38,10 @@ function multiply(a, b) {
 };
 
 function divide(a, b) {
+  if (b === 0) {
+    alert("Cannot divide by zero.");
+    return 0;
+  }
   return a / b
 };
 
@@ -20,9 +49,13 @@ function operate(operator, a, b) {
   operator(a, b)
 }
 
-let a = "";
-let b = "";
-let operator = "";
-let display = document.querySelector(".display")
+function onDisplayNumber(number) {
+  currentDisplay += number
+  display.textContent = currentDisplay
+  return currentDisplay
+}
 
+function updateDisplay() {
+  display.textContent = currentDisplay
+}
 
